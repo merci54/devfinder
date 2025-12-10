a9305edc1355dbb2cd7f4e1d83e989e4d47de1ee
Yaroslav <yaroslavlit@gmail.com>
2025-12-10 22:48:59 +0100

    add adaptive content block

--- src/components/ContentBlock/ContentBlock.tsx
+++ src/components/ContentBlock/ContentBlock.tsx
@@ -1,4 +1,3 @@
-// import { useUserStore } from "../../services/userStore";
 import Container from "../Container/Container";
 import css from "./ContentBlock.module.scss";
 
@@ -8,58 +7,56 @@ export default function ContentBlock() {
     <section className={css.section}>
       <Container>
         <div className={css.wrapper}>
-          <div className={css.header}>
+          <div className={css.imgBlock}>
             <img
-              className={css.header__img}
+              className={css.img}
               src="/icons/user-img.png"
               alt="user image"
             />
-            <div className={css.header__info}>
-              <h1 className={css.header__title}>The Octocat</h1>
-              <p className={css.header__username}>@octocat</p>
-              <p className={css.header__date}>Joined 25 Jan 2011</p>
-            </div>
           </div>
-          <div className={css.main}>
-            <p className={css.main__bio}>This profile has no bio</p>
-            <div className={css.stats}>
-              <ul className={css.stats__list}>
-                <li className={css.stats__item}>
-                  <span className={css.key}>Repos</span>
-                  <h2 className={css.value}>8</h2>
-                </li>
-                <li className={css.stats__item}>
-                  <span className={css.key}>Followers</span>
-                  <h2 className={css.value}>3938</h2>
-                </li>
-                <li className={css.stats__item}>
-                  <span className={css.key}>Following</span>
-                  <h2 className={css.value}>9</h2>
-                </li>
-              </ul>
-            </div>
-            <ul className={css.linksList}>
-              <li className={css.linksList__item}>
-                <img src="/icons/pin.svg" alt="pin icon" />
-                <span className={css.linksList__value}>San Francisco</span>
+          <div className={css.header}>
+            <h1 className={css.header__title}>The Octocat</h1>
+            <p className={css.header__date}>Joined 25 Jan 2011</p>
+            <p className={css.header__username}>@octocat</p>
+          </div>
+          <p className={css.bio}>This profile has no bio</p>
+          <div className={css.stats}>
+            <ul className={css.stats__list}>
+              <li className={css.stats__item}>
+                <span className={css.key}>Repos</span>
+                <h2 className={css.value}>8</h2>
               </li>
-              <li className={css.linksList__item}>
-                <img src="/icons/twitter.svg" alt="twitter icon" />
-                <span className={css.linksList__value}>Not Available</span>
+              <li className={css.stats__item}>
+                <span className={css.key}>Followers</span>
+                <h2 className={css.value}>3938</h2>
               </li>
-              <li className={css.linksList__item}>
-                <img src="/icons/url.svg" alt="url icon" />
-                <a className={css.linksList__value}>https://github.blog</a>
-              </li>
-              <li className={css.linksList__item}>
-                <img
-                  src="/icons/office-building.svg"
-                  alt="office building icon"
-                />
-                <span className={css.linksList__value}>@github</span>
+              <li className={css.stats__item}>
+                <span className={css.key}>Following</span>
+                <h2 className={css.value}>9</h2>
               </li>
             </ul>
           </div>
+          <ul className={css.linksList}>
+            <li className={css.linksList__item}>
+              <img src="/icons/pin.svg" alt="pin icon" />
+              <span className={css.linksList__value}>San Francisco</span>
+            </li>
+            <li className={css.linksList__item}>
+              <img src="/icons/twitter.svg" alt="twitter icon" />
+              <span className={css.linksList__value}>Not Available</span>
+            </li>
+            <li className={css.linksList__item}>
+              <img src="/icons/url.svg" alt="url icon" />
+              <a className={css.linksList__value}>https://github.blog</a>
+            </li>
+            <li className={css.linksList__item}>
+              <img
+                src="/icons/office-building.svg"
+                alt="office building icon"
+              />
+              <span className={css.linksList__value}>@github</span>
+            </li>
+          </ul>
         </div>
       </Container>
     </section>
